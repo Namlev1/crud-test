7217e0a171af9ac5c7c3427aaaa798e62f02ab9a
namlev <mikipatkowski@protonmail.com>
2024-12-02 17:58:13 +0100

    Change postgres to h2

---
+++ src/main/java/com/example/db/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.db.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
